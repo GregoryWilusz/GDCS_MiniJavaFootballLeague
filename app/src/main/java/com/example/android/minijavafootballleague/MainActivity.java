a4c2462d2668dd2566b3bb0f0b8bc5fc0e0c73dd
Grzegorz Wilusz <grzegorzwilusz@MacBook-Air-Grzegorz.local>
2017-12-04 22:00:18 +0100

    Added Logic of Games for both teams.

--- app/src/main/java/com/example/android/minijavafootballleague/MainActivity.java
+++ app/src/main/java/com/example/android/minijavafootballleague/MainActivity.java
@@ -10,7 +10,9 @@ import com.example.android.minijavafootballleague.R;
 public class MainActivity extends AppCompatActivity {
 
     int scoreTeamA = 0;
+    int gamesPlayedA = 0;
     int scoreTeamB = 0;
+    int gamesPlayedB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,17 +29,34 @@ public class MainActivity extends AppCompatActivity {
         scoreView.setText(String.valueOf(score));
     }
 
-    public void addThreeForTeamA(View view) {
+    /**
+     * Displays number of games played for Team A.
+     */
+    public void displayGamesTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_game);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void addWinTeamA(View view) {
         scoreTeamA += 3;
         displayForTeamA(scoreTeamA);
+        gamesPlayedA += 1;
+        displayGamesTeamA(gamesPlayedA);
+
     }
-    public void addTwoForTeamA(View view) {
-        scoreTeamA += 2;
-        displayForTeamA(scoreTeamA);
-    }
-    public void addOneForTeamA(View view) {
+    public void addDrawnTeamA(View view) {
         scoreTeamA += 1;
         displayForTeamA(scoreTeamA);
+        gamesPlayedA += 1;
+        displayGamesTeamA(gamesPlayedA);
+
+    }
+    public void lostTeamA(View view) {
+        scoreTeamA += 0;
+        displayForTeamA(scoreTeamA);
+        gamesPlayedA += 1;
+        displayGamesTeamA(gamesPlayedA);
+
     }
 
     /**
@@ -48,23 +67,41 @@ public class MainActivity extends AppCompatActivity {
         scoreView.setText(String.valueOf(score));
     }
 
-    public void addThreeForTeamB(View view) {
+    /**
+     * Displays number of games played for Team B.
+     */
+    public void displayGamesTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_game);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void addWinTeamB(View view) {
         scoreTeamB += 3;
         displayForTeamB(scoreTeamB);
+        gamesPlayedB += 1;
+        displayGamesTeamB(gamesPlayedB);
     }
-    public void addTwoForTeamB(View view) {
-        scoreTeamB += 2;
-        displayForTeamB(scoreTeamB);
-    }
-    public void addOneForTeamB(View view) {
+    public void addDrawnTeamB(View view) {
         scoreTeamB += 1;
         displayForTeamB(scoreTeamB);
+        gamesPlayedB += 1;
+        displayGamesTeamB(gamesPlayedB);
+    }
+    public void lostTeamB(View view) {
+        scoreTeamB += 0;
+        displayForTeamB(scoreTeamB);
+        gamesPlayedB += 1;
+        displayGamesTeamB(gamesPlayedB);
     }
 
     public void resetScore(View view) {
         scoreTeamA = 0;
+        gamesPlayedA = 0;
         displayForTeamA(scoreTeamA);
+        displayGamesTeamA(gamesPlayedA);
         scoreTeamB = 0;
+        gamesPlayedB = 0;
         displayForTeamB(scoreTeamB);
+        displayGamesTeamB(gamesPlayedB);
     }
 }
